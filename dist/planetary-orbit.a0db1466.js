d5eef59434e5abeae3120efe8bd16eb72612fdcb
varun_u <77634835+astroameba@users.noreply.github.com>
2021-04-03 11:53:23 -0400

    camera movement + panning

--- dist/planetary-orbit.a0db1466.js
+++ dist/planetary-orbit.a0db1466.js
@@ -167,7 +167,8 @@ window.addEventListener('DOMContentLoaded', function () {
   var engine = new BABYLON.Engine(canvas, true);
 
   var createScene = function createScene() {
-    var scene = new BABYLON.Scene(engine); //gets skybox assets/texture
+    var scene = new BABYLON.Scene(engine);
+    var selectedMesh; //gets skybox assets/texture
 
     var envTexture = BABYLON.CubeTexture.CreateFromImages([require("../../simAssets/skybox/milkyway/milkyway_px.jpg"), require("../../simAssets/skybox/milkyway/milkyway_py.jpg"), require("../../simAssets/skybox/milkyway/milkyway_pz.jpg"), require("../../simAssets/skybox/milkyway/milkyway_nx.jpg"), require("../../simAssets/skybox/milkyway/milkyway_ny.jpg"), require("../../simAssets/skybox/milkyway/milkyway_nz.jpg")], scene, false);
     scene.createDefaultSkybox(envTexture, false, 1000); //set arc rotate cam
@@ -177,7 +178,12 @@ window.addEventListener('DOMContentLoaded', function () {
     camera.lowerRadiusLimit = 2.1;
     camera.upperRadiusLimit = 650;
     camera.pinchPrecision = 100.0;
-    camera.wheelDeltaPercentage = 0.005; // Set up rendering pipeline
+    camera.wheelDeltaPercentage = 0.005;
+    camera.allowUpsideDown = true;
+    camera.panningAxis = new BABYLON.Vector3(1, 1, 0);
+    camera.panningInertia = .9; //camera.target = 
+    //console.log(camera);
+    // Set up rendering pipeline
 
     var pipeline = new BABYLON.DefaultRenderingPipeline("default", true, scene); //Anti-Aliasing (add setting to enable/disable this)
     // pipeline.samples = 4;
@@ -204,9 +210,10 @@ window.addEventListener('DOMContentLoaded', function () {
       return console.log(issue);
     }); //create Earth
 
-    var earth = BABYLON.Mesh.CreateSphere("earth", 32, 15.0, scene);
-    earth.position.x = 15;
-    earth.position.z = 15;
+    var earth = BABYLON.Mesh.CreateSphere("earth", 32, 1.0, scene);
+    earth.position.x = 5;
+    earth.position.z = 5;
+    earth.rotation = new BABYLON.Vector3(0, 0, Math.PI);
     earth.renderingGroupId = 3; //create earth's texture
 
     var earthMat = new BABYLON.StandardMaterial("earth-material", scene);
@@ -215,13 +222,50 @@ window.addEventListener('DOMContentLoaded', function () {
     earthMat.bumpTexture.level = 1.5;
     earthMat.specularColor = new BABYLON.Color3(0, 0, 0);
     earth.material = earthMat;
-    var sunlight = new BABYLON.PointLight("sunlight", new BABYLON.Vector3(0, 0, 0), scene);
+    var sunlight = new BABYLON.PointLight("sunlight", new BABYLON.Vector3(0, 0, 0), scene); //environment lighting
+
     var downLight = new BABYLON.HemisphericLight("downlight", new BABYLON.Vector3(0, 1, 0), scene);
     downLight.intensity = 0.2;
     downLight.includedOnlyMeshes.push(earth);
     var upLight = new BABYLON.HemisphericLight("uplight", new BABYLON.Vector3(0, -1, 0), scene);
     upLight.intensity = 0.2;
-    upLight.includedOnlyMeshes.push(earth); //loading screen
+    upLight.includedOnlyMeshes.push(earth); //make all meshes pickable
+
+    console.log(scene.meshes);
+
+    for (var i = 1; i < scene.meshes.length; i++) {
+      scene.meshes[i].isPickable = true;
+    }
+
+    function showWorldAxis(size) {
+      var makeTextPlane = function makeTextPlane(text, color, size) {
+        var dynamicTexture = new BABYLON.DynamicTexture("DynamicTexture", 50, scene, true);
+        dynamicTexture.hasAlpha = true;
+        dynamicTexture.drawText(text, 5, 40, "bold 36px Arial", color, "transparent", true);
+        var plane = BABYLON.Mesh.CreatePlane("TextPlane", size, scene, true);
+        plane.material = new BABYLON.StandardMaterial("TextPlaneMaterial", scene);
+        plane.material.backFaceCulling = false;
+        plane.material.specularColor = new BABYLON.Color3(0, 0, 0);
+        plane.material.diffuseTexture = dynamicTexture;
+        return plane;
+      };
+
+      var axisX = BABYLON.Mesh.CreateLines("axisX", [BABYLON.Vector3.Zero(), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, 0.05 * size, 0), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, -0.05 * size, 0)], scene);
+      axisX.color = new BABYLON.Color3(1, 0, 0);
+      var xChar = makeTextPlane("X", "red", size / 10);
+      xChar.position = new BABYLON.Vector3(0.9 * size, -0.05 * size, 0);
+      var axisY = BABYLON.Mesh.CreateLines("axisY", [BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(-0.05 * size, size * 0.95, 0), new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(0.05 * size, size * 0.95, 0)], scene);
+      axisY.color = new BABYLON.Color3(0, 1, 0);
+      var yChar = makeTextPlane("Y", "green", size / 10);
+      yChar.position = new BABYLON.Vector3(0, 0.9 * size, -0.05 * size);
+      var axisZ = BABYLON.Mesh.CreateLines("axisZ", [BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, -0.05 * size, size * 0.95), new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, 0.05 * size, size * 0.95)], scene);
+      axisZ.color = new BABYLON.Color3(0, 0, 1);
+      var zChar = makeTextPlane("Z", "blue", size / 10);
+      zChar.position = new BABYLON.Vector3(0, 0.05 * size, 0.9 * size);
+    }
+
+    ;
+    showWorldAxis(15); //loading screen
 
     engine.displayLoadingUI();
     scene.executeWhenReady(function () {
@@ -263,7 +307,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61837" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54181" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/simulations/planetaryOrbit/planetary-orbit.js
+++ src/simulations/planetaryOrbit/planetary-orbit.js
@@ -7,6 +7,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
     var createScene = function () {
         var scene = new BABYLON.Scene(engine);
+        var selectedMesh;
 
         //gets skybox assets/texture
         var envTexture = BABYLON.CubeTexture.CreateFromImages([require("../../simAssets/skybox/milkyway/milkyway_px.jpg"), 
@@ -22,8 +23,13 @@ window.addEventListener('DOMContentLoaded', function () {
         camera.upperRadiusLimit = 650;
         camera.pinchPrecision = 100.0;
         camera.wheelDeltaPercentage = 0.005;
+        camera.allowUpsideDown = true;
+        camera.panningAxis = new BABYLON.Vector3(1,1,0);
+        camera.panningInertia = .9;
+        //camera.target = 
+        //console.log(camera);
         
-        
+
         // Set up rendering pipeline
         var pipeline = new BABYLON.DefaultRenderingPipeline("default", true, scene);
 
@@ -53,9 +59,10 @@ window.addEventListener('DOMContentLoaded', function () {
         }).catch((issue) => console.log(issue));
 
         //create Earth
-        var earth = BABYLON.Mesh.CreateSphere("earth", 32, 15.0, scene);
-        earth.position.x = 15;
-        earth.position.z = 15;
+        var earth = BABYLON.Mesh.CreateSphere("earth", 32, 1.0, scene);
+        earth.position.x = 5;
+        earth.position.z = 5;
+        earth.rotation = new BABYLON.Vector3(0,0,Math.PI);
         earth.renderingGroupId = 3;
 
         //create earth's texture
@@ -68,12 +75,56 @@ window.addEventListener('DOMContentLoaded', function () {
 
         var sunlight = new BABYLON.PointLight("sunlight", new BABYLON.Vector3(0,0,0), scene);
 
+        //environment lighting
         var downLight = new BABYLON.HemisphericLight("downlight", new BABYLON.Vector3(0, 1, 0), scene);
         downLight.intensity = 0.2;
         downLight.includedOnlyMeshes.push(earth);
         var upLight = new BABYLON.HemisphericLight("uplight", new BABYLON.Vector3(0, -1, 0), scene);
         upLight.intensity = 0.2;
         upLight.includedOnlyMeshes.push(earth);
+
+        //make all meshes pickable
+        console.log(scene.meshes);
+        for (var i = 1; i < scene.meshes.length; i++){
+            scene.meshes[i].isPickable = true;
+        }
+
+
+        function showWorldAxis(size) {
+            var makeTextPlane = function(text, color, size) {
+                var dynamicTexture = new BABYLON.DynamicTexture("DynamicTexture", 50, scene, true);
+                dynamicTexture.hasAlpha = true;
+                dynamicTexture.drawText(text, 5, 40, "bold 36px Arial", color , "transparent", true);
+                var plane = BABYLON.Mesh.CreatePlane("TextPlane", size, scene, true);
+                plane.material = new BABYLON.StandardMaterial("TextPlaneMaterial", scene);
+                plane.material.backFaceCulling = false;
+                plane.material.specularColor = new BABYLON.Color3(0, 0, 0);
+                plane.material.diffuseTexture = dynamicTexture;
+            return plane;
+             };
+            var axisX = BABYLON.Mesh.CreateLines("axisX", [ 
+              BABYLON.Vector3.Zero(), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, 0.05 * size, 0), 
+              new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, -0.05 * size, 0)
+              ], scene);
+            axisX.color = new BABYLON.Color3(1, 0, 0);
+            var xChar = makeTextPlane("X", "red", size / 10);
+            xChar.position = new BABYLON.Vector3(0.9 * size, -0.05 * size, 0);
+            var axisY = BABYLON.Mesh.CreateLines("axisY", [
+                BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3( -0.05 * size, size * 0.95, 0), 
+                new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3( 0.05 * size, size * 0.95, 0)
+                ], scene);
+            axisY.color = new BABYLON.Color3(0, 1, 0);
+            var yChar = makeTextPlane("Y", "green", size / 10);
+            yChar.position = new BABYLON.Vector3(0, 0.9 * size, -0.05 * size);
+            var axisZ = BABYLON.Mesh.CreateLines("axisZ", [
+                BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3( 0 , -0.05 * size, size * 0.95),
+                new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3( 0, 0.05 * size, size * 0.95)
+                ], scene);
+            axisZ.color = new BABYLON.Color3(0, 0, 1);
+            var zChar = makeTextPlane("Z", "blue", size / 10);
+            zChar.position = new BABYLON.Vector3(0, 0.05 * size, 0.9 * size);
+        };
+        showWorldAxis(15);
 
 
         //loading screen
