698521183dee774ce05a5fddad34b02811125d3a
varun unnithan <77634835+varun-un@users.noreply.github.com>
2021-08-19 23:03:36 -0400

    set up sim cards

--- src/site/sim-cards.jsx
+++ src/site/sim-cards.jsx
@@ -1,6 +1,45 @@
-import { h, render, Component } from 'preact'
+import { h, render, Component, Fragment } from 'preact'
+import { useState, useEffect } from 'preact/hooks'
+/** @jsx h */
+
 import './css/sim-card.css'
 
+
+/**
+ * @typedef {Object} SimulationDetails
+ * @property {string} title - The title of the simulation
+ * @property {string} creator - The creator(s) of the simulation
+ * @property {string} description - A short description of the simulation (under 180 characters)
+ * @property {string} grades - The range of grades levels that the simulation is suitable for
+ * @property {string} fileLocation - The location of the simulation's start HTML file (relative to this file)
+ * @property {any} imageLocation - The path to the thumbnail image of the simulation (relative to this file) passed into require()
+ * @property {string} color - The color to use for the simulation's card
+**/
+
+/**
+ * Add new simulations' details here
+ * @type {Array.<SimulationDetails>} - An array of SimulationDetails objects
+**/
+var simulations = [
+    {
+        title: "Planetary Orbit",
+        creator: "Varun Unnithan",
+        description: "Learn about the shape a planet's orbit follows, and the features of its orbit.",      
+        grades: "8 - 11",
+        fileLocation: "../__/simulations/planetaryOrbit/planetary-orbit.html",
+        imageLocation: require("./thumbnails/nike_shoe.png"),
+        color: "#bbbbbb"
+    },
+    {
+        title: "Planetary Orbit",
+        creator: "Varun Unnithan",
+        description: "Learn about the shape a planet's orbit follows, and the features of its orbit.",
+        grades: "8 - 11",
+        fileLocation: "../simulations/planetaryOrbit/planetary-orbit.html",
+        imageLocation: require("./thumbnails/nike_shoe.png"),
+        color: "#ff0000"
+    }
+]
 
 function SimCard(props) {
     return (
@@ -30,11 +69,12 @@ function SimCard(props) {
 }
 
 render(
-
-    <SimCard title="Planetary Orbits" grades="8-10" link="../__/simulations/planetaryOrbit/planetary-orbit.html" 
-    img={require("./thumbnails/nike_shoe.png")} color="#00ff00">
-        Desc: This is a test rly long desc cuz I do not like to sdjflk sdlk kajshfjk sf dsjfkjsd sdgj jkg hfd dfjgdkjfg 
-        ddkfj gd fjgh jkdf fdg kjdfj.
-    </SimCard>
-
+    <Fragment>
+        {simulations.map(sim => (
+            <SimCard title={sim.title} img={sim.imageLocation} color={sim.color} grades={sim.grades} 
+                link={sim.fileLocation}>
+                    {sim.description}
+            </SimCard>
+        ))}
+    </Fragment>
 , document.body)
